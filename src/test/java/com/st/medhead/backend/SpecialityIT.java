36c24479980f7446aad5cd938664d3d78f1eec1c
TUAL <stual37@icloud.com>
2024-03-05 15:01:24 +0100

    Fin projet

--- src/test/java/com/st/medhead/backend/SpecialityIT.java
+++ src/test/java/com/st/medhead/backend/SpecialityIT.java
@@ -1,5 +1,9 @@
 package com.st.medhead.backend;
 
+import org.junit.*;
+import org.springframework.beans.factory.annotation.*;
+
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,8 +12,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,30 +24,37 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.st.medhead.backend.controller.SpecialityController;
 
 import com.st.medhead.backend.model.Hospital;
-
+import com.st.medhead.backend.model.Speciality;
+import com.st.medhead.backend.service.SpecialityService;
 
 import jakarta.inject.Inject;
 
-@WebMvcTest(controllers = {SpecialityController.class})
+//@WebMvcTest(controllers = {SpecialityController.class})
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SpecialityIT {
 	@Inject
     private MockMvc mockMvc;
-
-	
-	
+/*
+	@MockBean
+    private SpecialityService specialityService;
+	*/
+		
 	private Hospital hospital;
 	
-	@Autowired
-	SpecialityController specialityController;
+	
 	
 	@Test
 	public void givenASpecialities() throws Exception {
-				
+		
+		//Iterable<Speciality> specialities;
+		//given(this.specialityService.getSpecialities()).willReturn(specialities);		
 		mockMvc.perform(get("/specialities"))
 		.andExpect(status()
 		.is2xxSuccessful())
 		.equals(hospital);
+		
 	}
 	
 	@Test
@@ -50,5 +64,6 @@ public class SpecialityIT {
 		.andExpect(status()
 		.is2xxSuccessful())
 		.equals(hospital);
+		
 	}
 }
